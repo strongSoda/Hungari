80b2740bbfa89df7bbc8ab12da0c35a2ff631f2d
strongSoda <strongsoda2@gmail.com>
2019-01-15 17:39:35 +0530

    adding component structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h3>hello from navbar</h3>
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h3>hello from product</h3>
+            </div>
+
+         );
+    }
+}
+ 
+export default Product;
