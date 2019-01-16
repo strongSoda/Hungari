9b31accfcda5b11b5eb01c537fdd5d43a94f4ee0
strongSoda <strongsoda2@gmail.com>
2019-01-16 18:07:22 +0530

    setup Title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
 
 class ProductList extends Component {
-    state = {  }
+    state = { 
+        products: []
+     }
     render() { 
-        return (  
-            <div>
-                <Product></Product>
-            </div>
+        return (
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products"></Title>
+                        <div className="row">
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>  
+                // <Product></Product>
         );
     }
 }
