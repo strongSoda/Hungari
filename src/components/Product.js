c2b56e39f1449d73f20e6a3dc043e28458bf373c
strongSoda <strongsoda2@gmail.com>
2019-01-16 20:14:11 +0530

    styled add to cart btn complete

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,7 +18,7 @@ class Product extends Component {
                         <Link to='/details'>
                             <img src={img} alt={title} className="card-img-top"></img>
                         </Link>
-                        <button className="btn-cart" disabled={inCart? true: false} 
+                        <button className="card-btn" disabled={inCart? true: false} 
                          onClick={ () => { console.log('added to the cart') }}>
                         {inCart?
                         (<p className="text-capitalize mb-0" disabled>
@@ -47,5 +47,53 @@ class Product extends Component {
 export default Product;
 
 const ProductWrapper = styled.div`
+    .card{
+        border-color: transparent;
+        transition: all 1s linear;
+    }
+    .card-footer{
+        background: transparent;
+        border-top: transparent;
+        transition: all 0.3s linear;
+    }
+    &:hover{
+        .card{
+            border: 0.04rem solid rgba(0,0,0,0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+        }
+        .card-footer{
+            background: rgba(247,247,247);
+        }
+    }
 
+    .img-container{
+        position: relative;
+        overflow: hidden;
+    }
+    .card-img-top {
+        transition: all 0.5s linear;
+    }
+    .img-container:hover .card-img-top {
+        transform: scale(1.2);
+    }
+    .card-btn{
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 0.2rem 0.4rem;
+        background: #009ffd;
+        border: none;
+        color: #f3f3f3;
+        font-size: 1.4rem;
+        border-radius: 0.5rem 0rem 0rem 0rem;
+        transform: translate(100%,100%);
+        transition: all 0.5s linear;
+    }
+    .img-container:hover .card-btn {
+        transform: translate(0,0);
+    }
+    .card-btn:hover{
+        color: #2a2a72;
+        cursor: pointer;
+    }
 `;
